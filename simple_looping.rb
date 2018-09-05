4a442ace1df4c0f5837fb2f9376da023bb090ae9
Iman Jawad <imanj12@gmail.com>
2018-09-05 20:06:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -57,6 +57,6 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  for counter in 
+  for counter in 1..number_of_times
 end
 
